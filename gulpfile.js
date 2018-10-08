23c6536a3db832b778d26ca12bd7952655a713c5
mandarsd <mandar@ebi.ac.uk>
2018-10-08 14:32:38 +0100

    Screenshot button issue fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,24 @@
 
 var gulp = require("gulp");
 
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
+// Include CSS components
 var prefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
-var del = require("del");
-var bump = require("gulp-bump");
-var git = require("gulp-git");
+var minifycss = require('gulp-minify-css');
+
+// Include JS components
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Include utilities
 var rename = require("gulp-rename");
 var header = require('gulp-header');
+
+// var del = require("del");
+// var bump = require("gulp-bump");
+// var git = require("gulp-git");
+// var rename = require("gulp-rename");
+// var header = require('gulp-header');
 
 var srcBundle = [
 	"lib/js/es6-promise.min.js",
@@ -55,9 +64,9 @@ var license = ['/**',
   ''].join('\n');
 
 
-gulp.task("clean", function (cb) {
-    del('[build]', cb);
-});
+// gulp.task("clean", function (cb) {
+//     del('[build]', cb);
+// });
 
 gulp.task("scriptsComplete", function () {
     return gulp.src(srcBundle)
@@ -81,33 +90,35 @@ gulp.task('stylesComplete', function () {
     gulp.src(srcCss + '/*.css')
         .pipe(prefixer('last 5 versions'))
         .pipe(concat('pdb.litemol.min.css'))
-        .pipe(cleanCSS())
+        .pipe(minifycss())
 		.pipe(header(license, {} ))
         .pipe(gulp.dest("./build"));
 });
 
-gulp.task("bump", function(){
-   return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump())
-    .pipe(gulp.dest('./')); 
-});
+// gulp.task("bump", function(){
+//    return gulp.src(['./package.json', './bower.json'])
+//     .pipe(bump())
+//     .pipe(gulp.dest('./')); 
+// });
 
-gulp.task("tag", function(){
-    var pkg = require('./package.json');
-    var v = pkg.version;
-    var message = 'Release ' + v;
+// gulp.task("tag", function(){
+//     var pkg = require('./package.json');
+//     var v = pkg.version;
+//     var message = 'Release ' + v;
     
-    return gulp.src('./')
-        .pipe(git.commit(message))
-        .pipe(git.tag(v, message))
-        .pipe(git.push('origin', 'master', '--tags'))
-        .pipe(gulp.dest('./'));
-})
+//     return gulp.src('./')
+//         .pipe(git.commit(message))
+//         .pipe(git.tag(v, message))
+//         .pipe(git.push('origin', 'master', '--tags'))
+//         .pipe(gulp.dest('./'));
+// })
 
-gulp.task('npm', ['bump','tag'], function (done) {
-  require('child_process').spawn('npm', ['publish'], { stdio: 'inherit' })
-    .on('close', done);
-});
+// gulp.task('npm', ['bump','tag'], function (done) {
+//   require('child_process').spawn('npm', ['publish'], { stdio: 'inherit' })
+//     .on('close', done);
+// });
 
-gulp.task("default", ["clean", "scriptsCore", "scriptsComplete", "stylesComplete"]);
+// gulp.task("default", ["clean", "scriptsCore", "scriptsComplete", "stylesComplete"]);
 //gulp.task("release",['default','npm']);
+
+gulp.task('default', ["scriptsCore", "scriptsComplete", "stylesComplete"]);
